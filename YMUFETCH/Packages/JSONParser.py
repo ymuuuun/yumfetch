3f310d13889dda0ec2701f5edc29c4064ac1a590
yummy <whathowdudebrooo@gmail.com>
2024-07-09 16:22:49 -0700

    Files added.

---
+++ YMUFETCH/Packages/JSONParser.py
@@ -0,0 +1,14 @@
+import json
+import pathlib
+
+RootFolder = pathlib.Path(__file__).parent.parent
+
+def parseConfigFile() -> dict:
+    try:
+        with open(f"{RootFolder}\\Config.json") as file:
+            Parsed = json.loads(file.read())
+            return Parsed
+    except Exception as err:
+        raise Exception(err)
+    
+# parseConfigFile()
